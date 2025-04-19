616d1487969eccb973864a4ef38cd956f9bba993
Maurício Mafra Monnerat <monnerat.mauricio@gmail.com>
2025-04-18 21:42:41 -0300

    removed tile spawning to tile replacement, added rotation to vehicle

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,11 @@ pub struct MainCar();
 #[derive(Clone, Component)]
 pub struct OpponentCar();
 
-#[derive(Clone, Component)]
+#[derive(Component)]
 pub struct Border();
+
+#[derive(Component)]
+pub struct DebugComponent();
 
 #[derive(Clone, Component)]
 pub struct Background();
@@ -22,6 +25,16 @@ impl Default for CarSpawnTimer {
         return Self(Timer::new(TimerType::Repeat, Duration::new(1, 0)))
     }
 }
+
+//#[derive(Resource)]
+//pub struct ScoreTimer(pub Timer);
+//
+//impl Default for ScoreTimer {
+//    fn default() -> Self {
+//        return Self(Timer::new(TimerType::Once, Duration::new));
+//    }
+//}
+
 
 #[derive(Clone, Resource)]
 pub struct CarSprites(pub Vec<String>);
@@ -36,10 +49,13 @@ impl Default for CarSprites {
     }
 }
 
+#[derive(Clone, Resource)]
+pub struct BackgroundTileCounter(pub i32);
+
 your_game!(
     WindowConfiguration {
         icon_path: "sprites/64x64/cars/white-lancer.png".to_string(),
-        title: "Scarlet Eyes: Fury on the Road".to_string(),
+        title: "Lancer TURBO: Fury on the Road".to_string(),
         background_color: Some(Color::BLACK),
         background_image_path: None,
         width: 960.0,
@@ -59,8 +75,8 @@ your_game!(
 fn setup(context: &mut Context) {
     let white_lancer_sprite: Sprite = Sprite::new("sprites/64x64/cars/white-lancer.png".to_string());
 
-    //let border_left_shape: Shape = Shape::new(Orientation::Horizontal, GeometryType::Rectangle, Color::RED);
-    //let border_right_shape: Shape = Shape::new(Orientation::Horizontal, GeometryType::Rectangle, Color::RED);
+    let border_left_shape: Shape = Shape::new(Orientation::Horizontal, GeometryType::Rectangle, Color::RED);
+    let border_right_shape: Shape = Shape::new(Orientation::Horizontal, GeometryType::Rectangle, Color::RED);
 
     //let test: Shape = Shape::new(Orientation::Horizontal, GeometryType::Square, Color::RED);
     //context.commands.spawn(
@@ -70,6 +86,7 @@ fn setup(context: &mut Context) {
     //    ]
     //);
 
+    // Background Tiles
     context.commands.spawn(
         vec![
             Box::new(Sprite::new("sprites/960x600/worlds/night-bridge.png".to_string())),
@@ -78,7 +95,6 @@ fn setup(context: &mut Context) {
             Box::new(DrawOrder(0))
         ]
     );
-
     context.commands.spawn(
         vec![
             Box::new(Sprite::new("sprites/960x600/worlds/night-bridge.png".to_string())),
@@ -87,7 +103,6 @@ fn setup(context: &mut Context) {
             Box::new(DrawOrder(0))
         ]
     );
-
     context.commands.spawn(
         vec![
             Box::new(Sprite::new("sprites/960x600/worlds/night-bridge.png".to_string())),
@@ -99,7 +114,8 @@ fn setup(context: &mut Context) {
 
     context.commands.add_resources(vec![
         Box::new(CarSpawnTimer::default()),
-        Box::new(CarSprites::default())
+        Box::new(CarSprites::default()),
+        Box::new(BackgroundTileCounter(0))
     ]);
 
     context.commands.spawn(
@@ -115,65 +131,72 @@ fn setup(context: &mut Context) {
 
     context.commands.spawn(
         vec![
-            //Box::new(border_left_shape),
+            Box::new(border_left_shape),
             Box::new(Transform::new(Vector2::new(0.5, 0.0), 0.0, Vector2::new(0.01, 5.0))),
             Box::new(Border()),
+            Box::new(DebugComponent()),
+            Box::new(Visibility{value: false}),
+            Box::new(DrawOrder(4)),
             Box::new(Collision::new(Collider::new_simple(GeometryType::Rectangle)))
         ]
     );
 
     context.commands.spawn(
         vec![
-            //Box::new(border_right_shape),
+            Box::new(border_right_shape),
             Box::new(Transform::new(Vector2::new(-0.5, 0.0), 0.0, Vector2::new(0.01, 5.0))),
             Box::new(Border()),
+            Box::new(DebugComponent()),
+            Box::new(Visibility{value: false}),
+            Box::new(DrawOrder(4)),
             Box::new(Collision::new(Collider::new_simple(GeometryType::Rectangle)))
         ]
     );
 }
 
 fn update(context: &mut Context) {
-
     let player_entity: Entity = {
         let mut player_entity_query: Query = Query::new(&context.world).with::<MainCar>();
         player_entity_query.entities_with_components().unwrap().first().unwrap().clone()
     };
 
-    handle_background_spawn(context);
+    handle_background_tiles(context);
     move_background(context);
-
-    let input: ResourceRef<'_, Input> = context.world.get_resource::<Input>().unwrap();
-
-    move_player(context, player_entity, input);
+    handle_input(context, player_entity);
     handle_opponents_movement(context);
     check_player_border_collision(context, player_entity);
     check_player_opponent_collision(context, player_entity);
     spawn_opponent(context);
 }
 
-fn handle_background_spawn(context: &mut Context) {
+fn handle_input(context: &Context, player_entity: Entity) {
+    let input: ResourceRef<'_, Input> = context.world.get_resource::<Input>().unwrap();
+
+    move_player(context, player_entity, &input);
+    set_debug_visibility(context, &input);
+}
+
+fn handle_background_tiles(context: &Context) {
     let background_entities: Vec<Entity> = {
         let mut background_query: Query = Query::new(&context.world).with::<Background>();
         background_query.entities_with_components().unwrap()
     };
 
-    let first_background_entity: Entity =  background_entities.first().unwrap().clone();
-    let last_background_entity: Entity =  background_entities.last().unwrap().clone();
+    let mut tile_counter: ResourceRefMut<'_, BackgroundTileCounter> = context.world.get_resource_mut::<BackgroundTileCounter>().unwrap();
 
-    let f_bg_transform: ComponentRef<'_, Transform> = context.world.get_entity_component::<Transform>(&first_background_entity).unwrap();
+    let first_tile_index: usize = tile_counter.0 as usize;
+    let last_tile_intex: usize = (first_tile_index + 2)%3;
+
+    let first_background_entity: Entity =  background_entities.get(first_tile_index).unwrap().clone();
+    let last_background_entity: Entity =  background_entities.get(last_tile_intex).unwrap().clone();
+
+    let mut f_bg_transform: ComponentRefMut<'_, Transform> = context.world.get_entity_component_mut::<Transform>(&first_background_entity).unwrap();
     let l_bg_transform: ComponentRef<'_, Transform> = context.world.get_entity_component::<Transform>(&last_background_entity).unwrap();
 
-    if f_bg_transform.get_position().y <= -1.99 {
-        let spawn_y: f32 = 1.9 + l_bg_transform.get_position().y;
-        context.commands.spawn(
-            vec![
-                Box::new(Sprite::new("sprites/960x600/worlds/night-bridge.png".to_string())),
-                Box::new(Transform::new(Vector2::new(0.005, spawn_y), 0.0, Vector2::new(1.55, 1.0))),
-                Box::new(DrawOrder(0)),
-                Box::new(Background())
-            ]
-        );
-        context.commands.despawn(first_background_entity);
+    if f_bg_transform.get_position().y <= -2.0 {
+        let new_y: f32 = 2.0 + l_bg_transform.get_position().y;
+        f_bg_transform.set_position_y(&context.render_state, new_y);
+        tile_counter.0 = (tile_counter.0+1)%3;
     }
 }
 
@@ -191,18 +214,39 @@ fn move_background(context: &Context) {
     }
 }
 
-fn move_player(context: &Context, player_entity: Entity, input: ResourceRef<'_, Input>) {
+fn move_player(context: &Context, player_entity: Entity, input: &ResourceRef<'_, Input>) {
     let mut transform: ComponentRefMut<'_, Transform> = context.world.get_entity_component_mut::<Transform>(&player_entity).unwrap();
     let car_speed: ComponentRef<'_, Velocity> = context.world.get_entity_component::<Velocity>(&player_entity).unwrap();
 
     if input.is_key_pressed(PhysicalKey::Code(KeyCode::KeyA)) {
         let move_left: f32 = transform.position.x - car_speed.value.x * context.delta;
         transform.set_position_x(&context.render_state, move_left);
+        transform.set_rotation(&context.render_state, 10.0);
     }
 
     if input.is_key_pressed(PhysicalKey::Code(KeyCode::KeyD)) {
         let move_right: f32 = transform.position.x + car_speed.value.x * context.delta;
         transform.set_position_x(&context.render_state, move_right);
+        transform.set_rotation(&context.render_state, -10.0);
+    }
+
+    if input.is_key_released(PhysicalKey::Code(KeyCode::KeyA))
+    || input.is_key_released(PhysicalKey::Code(KeyCode::KeyD)) {
+        transform.set_rotation(&context.render_state, 0.0);
+    }
+    
+}
+
+fn set_debug_visibility(context: &Context, input: &ResourceRef<'_, Input>) {
+    // Alterar a visibilidade das entidades com o componente debug
+    if input.is_key_released(PhysicalKey::Code(KeyCode::Semicolon)) {
+        let mut debug_query: Query = Query::new(&context.world).with::<DebugComponent>();
+        let debug_entities: Vec<Entity> = debug_query.entities_with_components().unwrap();
+    
+        for debug_entity in &debug_entities {
+            let mut visibility_component: ComponentRefMut<'_, Visibility> = context.world.get_entity_component_mut(debug_entity).unwrap();
+            visibility_component.value = !visibility_component.value;
+        }
     }
 }
 
@@ -245,7 +289,7 @@ fn handle_opponents_movement(context: &mut Context) {
     for opponent in opponents_entities {
         let mut op_transform: ComponentRefMut<'_, Transform> = context.world.get_entity_component_mut::<Transform>(&opponent).unwrap();
 
-        if op_transform.get_position().y < -1.5 {
+        if op_transform.get_position().y < -1.3 {
             eprintln!("opponent despawned: {:?}", opponent.0);
             context.commands.despawn(opponent);
         } else {
@@ -265,7 +309,7 @@ fn spawn_opponent(context: &mut Context) {
 
     if timer_finished {
         let mut thread_rng: ThreadRng = rand::rng();
-        let number_of_opponents_to_spawn: i32 = thread_rng.random_range(1..=3);
+        let number_of_opponents_to_spawn: i32 = thread_rng.random_range(1..=5);
         let car_sprites: CarSprites = {
             let car_sprites_ref: ResourceRef<'_, CarSprites> = context.world.get_resource::<CarSprites>().unwrap();
             car_sprites_ref.clone()
@@ -273,7 +317,7 @@ fn spawn_opponent(context: &mut Context) {
 
         eprintln!("number of opponents to spawn: {:?}", number_of_opponents_to_spawn);
         
-        let mut lane_numbers: Vec<i32> = vec![1, 2, 3, 4];
+        let mut lane_numbers: Vec<i32> = vec![1, 2, 3, 4, 5, 6];
         for _ in 1..=number_of_opponents_to_spawn {
             let lane_index: usize = thread_rng.random_range(0..lane_numbers.len()) as usize;
             let lane_number: i32 = lane_numbers.remove(lane_index);
@@ -281,24 +325,46 @@ fn spawn_opponent(context: &mut Context) {
 
             let car_sprite: &String = car_sprites.0.choose(&mut thread_rng).unwrap();
             let car_offset: f32 = thread_rng.random_range(-0.9..=0.9);
-            spawn_opponent_car(context, lane_number, car_sprite, car_offset);
+            let spawn_position: Vector2<f32> = calculate_car_spawn_position(lane_number, car_offset);
+            spawn_opponent_car(context, spawn_position, car_sprite);
         }
     }
 }
 
-fn spawn_opponent_car(context: &mut Context, lane_number: i32, car_srpite_path: &String, car_offset: f32) {
-    let lane_number_float: f32 = lane_number as f32;
-    let x_position: f32 = (0.7/3.0)*lane_number_float - (7.0/12.0);
-    let y_position: f32 = 3.0 + car_offset;
-    eprintln!("opponent spawned at: {:?},{:?}", x_position, y_position);
+fn spawn_opponent_car(context: &mut Context, spawn_position: Vector2<f32>, car_srpite_path: &String) {
+    eprintln!("opponent spawned at: {:?},{:?}", spawn_position.x, spawn_position.y);
     context.commands.spawn(
         vec![
             Box::new(Sprite::new(car_srpite_path.to_string())),
-            Box::new(Transform::new(Vector2::new(x_position, y_position), 0.0, Vector2::new(0.08, 0.08))),
+            Box::new(Transform::new(spawn_position, 0.0, Vector2::new(0.08, 0.08))),
             Box::new(OpponentCar()),
             Box::new(Velocity::new(Vector2::new(0.0, -2.0))),
             Box::new(Collision::new(Collider::new_simple(GeometryType::Rectangle))),
             Box::new(DrawOrder(2))
         ]
     );
+}
+
+fn calculate_car_spawn_position(lane_number: i32, car_offset: f32) -> Vector2<f32> {
+    let lane_number_float: f32 = lane_number as f32;
+    // spawn_range represents the total range in x axis which cars can be spawned,
+    // for example: with spawn_range = 0.8, a car can be spawned between x = -0.4 and x = 0.4
+    let spawn_range: f32 = 0.75;
+
+    // f(n) = an + b
+    // with 'n' as lane number (which varies from 1..6)
+    // so in the first lane (n=1), the car would be spawned on the left border (negative part on x axis), having:
+    // 1*a + b = (-1.0)*(spawn_range/2.0)
+    // 
+    // and in the last lane (n=6), the car would be spawned on the right border (positive part on x axis), having:
+    // 6*a + b = (spawn_range/2.0)
+    // 
+    // so we get:
+    // | a + b = -spawn_range/2.0
+    // | 6a + b = spawn_range/2.0
+    // with some function manipulation, we get: 5a = spawn_range
+    let a: f32 = spawn_range/5.0;
+    let b: f32 = spawn_range/(-2.0) - a;
+
+    return Vector2::new(a*lane_number_float + b, 3.0 + car_offset);
 }
